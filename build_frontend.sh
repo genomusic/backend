4d7dbbd928b685da71b0ec9d353a14b8f7c2f887
Kfir Stri <kfirstar@gmail.com>
2018-08-22 22:18:04 +0300

    added copy

--- build_frontend.sh
+++ build_frontend.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-npm run-script build && cp -r ./frontend/build/ ./static/ && ls ./static/
+npm run-script build && cp -r ./frontend/build/* ./static/ && ls ./static/ && cp ./static/index.html ./templates/
