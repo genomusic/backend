69fd7e581cd1950b964acb15701b9ca1be4f87de
Kfir Stri <kfirstar@gmail.com>
2018-08-25 18:09:45 +0300

    fix stuff

--- build_frontend.sh
+++ build_frontend.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-npm run-script build && cp -r ./frontend/build/* ./static/ && ls -r ./static/* && cp ./static/index.html ./templates/
+#npm run-script build && cp -r ./frontend/build/* ./static/ &&
+ls -r ./static/* # && cp ./static/index.html ./templates/
